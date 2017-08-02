b13162d1f0cb80856063c4f0aeff6e8ff800c1d0
Marco Lagalla <marcolagalla@yahoo.it>
2017-08-02 10:59:44 +0200

    commit

---
+++ Progetto O/src/progetto/o/Votazione.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progetto.o;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author marco
+ */
+public class Votazione {
+    
+    private String idVotazione;
+    
+    public Votazione(String _idVotazione) { // costruttore
+        this.idVotazione = _idVotazione;
+    }
+    
+    
+    public apriVotazione() {
+        
+    }
+    
+}
