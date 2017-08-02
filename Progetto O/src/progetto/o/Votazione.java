03055e40639543adf19b5ae903b1103ea8ef008b
Marco Lagalla <marcolagalla@yahoo.it>
2017-08-02 12:33:48 +0200

    Aggiunte alla classe Votazioni

--- Progetto O/src/progetto/o/Votazione.java
+++ Progetto O/src/progetto/o/Votazione.java
@@ -8,24 +8,39 @@ package progetto.o;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-/**
- *
- * @author marco
- */
 public class Votazione {
+
+/*____________________________________STATO INTERNO________________________________________*/
     
     private String idVotazione;
     private final DateFormat f = new SimpleDateFormat("dd-MM-yyyy");
     private String dataCorrente;
     private String dataInizioVot;
     private String dataFineVot;
+    private MySQlConnection mysql = new MySQlConnection();
     
-    public Votazione(String _idVotazione, String dataFine) { // costruttore
+/*____________________________________COSTRUTTORI__________________________________________*/
+    
+    public Votazione(String _idVotazione, String dataFine) { // il costruttore crea una tabella nel db, rileva la data corrente e definisce lo stato interno
         this.idVotazione = _idVotazione;
-        Calendar cal = Calendar.getInstance();
-        String data = f.format(cal);
-        this.dataInizioVot = dataCorrente;
-        this.dataFineVot = dataFine;
+        
+        try {   // 
+            int res = mysql.UpdateQuery("CREATE TABLE " + idVotazione + "(Data String, NumeroVoti int)");
+                       if (res == 0 ) {
+                           System.out.println("errore query");
+                       }
+            Calendar cal = Calendar.getInstance();
+            String data = f.format(cal);
+            this.dataInizioVot = dataCorrente;
+            this.dataFineVot = dataFine;
+        } catch (Exception ex) {ex.printStackTrace();}
     }
+    
+/*_______________________________________METODI____________________________________________*/
+    
+    public void chiudiVotazione() {} 
+    public void addVoto() {}
 }
